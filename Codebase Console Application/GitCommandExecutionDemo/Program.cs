53d71b6149631a54a6260a54ed34682d2ad7bf4a
Jacob Huber <jhuber11235@gmail.com>
2020-10-29 16:14:33 -0700

    Blue print for parsing out most data from git log

--- Codebase Console Application/GitCommandExecutionDemo/Program.cs
+++ Codebase Console Application/GitCommandExecutionDemo/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
@@ -14,29 +17,51 @@ namespace GitCommandExecutionDemo
         {
             Console.WriteLine("Git Command Execution in C# Demo!");
 
-            string directory = Directory.GetCurrentDirectory();
+            /*string directory = Directory.GetCurrentDirectory();
 
             string git = "git";
             string gitLogArg = @"log";
 
-           Console.WriteLine(System.Diagnostics.Process.Start(git, gitLogArg))
+            Dictionary<int, string> commits = new Dictionary<int, string>();
+            int index = 1;
 
-            /*Collection<PSObject> results;
-            using (PowerShell powershell = PowerShell.Create())
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = true;
+            process = Process.Start(git, gitLogArg);*/
+
+            string output = "";
+            List<string> lines = new List<string>();
+            var proc = new Process
             {
-                powershell.AddScript($"cd {directory}");
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "git",
+                    Arguments = "log",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
 
-                powershell.AddScript(@"git init");
-                powershell.AddScript(@"git log");
-                results = powershell.Invoke();
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                output += line + "\n";
+                lines.Add(line);
             }
+            Console.WriteLine(output);
 
-            foreach (PSObject ps in results)
+            
+            List<string> commits = new List<string>();
+            foreach (string line in lines)
             {
-                Console.WriteLine(ps.ToString());
-            }*/
-
-
+                if (line.StartsWith("commit"))
+                {
+                    commits.Add(line.Substring(7, 40));
+                }
+            }
+            commits.ForEach(commit => { Console.WriteLine("commit: " + commit); });
         }
     }
 }
