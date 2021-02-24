a0799517e633150a7eb99849d1d16ec24dfad70a
Alex Udodik <alex.udodik@wsu.edu>
2021-02-23 19:08:18 -0800

    Fixed local sql access dependencies

--- CodebaseView/CodebaseView/SQL/SQL.cs
+++ CodebaseView/CodebaseView/SQL/SQL.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +11,39 @@ namespace CodebaseView
 {
     public static class SQL
     {
-        public static readonly string connectionString = "Host = localhost; Username = postgres; Database = 421Db; password = password";
+        public static readonly string connectionString = "Host = localhost; Port = 5435; Username = postgres; Database = 421Db; password = password";
 
         public static DataTable execute(string sqlstr)
         {
             using (var connection = new NpgsqlConnection(connectionString))
             {
-                connection.Open();
-                using (var cmd = new NpgsqlCommand(sqlstr, connection))
+                try
                 {
-                    try
+                    connection.Open();
+                    using (var cmd = new NpgsqlCommand(sqlstr, connection))
                     {
-                        DataTable dt = new DataTable();
-                        NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
-                        da.Fill(dt);
-                        return dt;
+                        try
+                        {
+                            DataTable dt = new DataTable();
+                            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                            da.Fill(dt);
+                            return dt;
+                        }
+                        catch (NpgsqlException e)
+                        {
+                            Console.WriteLine(e.Message.ToString());
+                            MessageBox message = new MessageBox("SQL Error: " + e.Message.ToString() + "\n Failing Query: " + sqlstr);
+                            message.Show();
+                            return null;
+                        }
+
                     }
-                    catch (NpgsqlException e)
-                    {
-                        Console.WriteLine(e.Message.ToString());
-                        MessageBox message = new MessageBox("SQL Error: " + e.Message.ToString() + "\n Failing Query: " + sqlstr);
-                        message.Show();
-                        return null;
-                    }
+                }
+                
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("File not found." + e.ToString());
+                    return null;
                 }
             }
         }
