3bd640fd5d1bd0ff84400f5ab5ffc50b07ba2113
Malachi Potts <malachi.potts@wsu.edu>
2021-04-08 22:10:45 -0700

    Added test for parseNameFromURL

--- CodebaseView/IDT_Tests/UnitTest1.cs
+++ CodebaseView/IDT_Tests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace IDT_Tests
     public class Tests
     {
         public const string realRepoURL = "https://gitlab.eecs.wsu.edu/2019080728/cpts-421.git";
+        public const string repoName = "cpts-421";
         [SetUp]
         public void Setup()
         {
@@ -19,10 +20,14 @@ namespace IDT_Tests
             GitParser gitparser = new GitParser();
             string repoURL = gitparser.retrieveRepoURL();
             Assert.AreEqual(repoURL, realRepoURL);
+        }
 
-
-            Console.WriteLine(repoURL);
-            //Assert.Pass();
+        [Test]
+        public void Test2()
+        {
+            GitParser gitParser = new GitParser();
+            string parsedName = gitParser.parseNameFromURL(realRepoURL);
+            Assert.AreEqual(repoName, parsedName);
         }
     }
 }
