16f1fb83d08a3b033d9be202d513a82c70379c2b
Alex Udodik <alex.udodik@wsu.edu>
2021-04-15 18:59:18 -0700

    Added api url executor and folder for api response objects

---
+++ CodebaseView/CodebaseView/Git REST API/API Objects/Branch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebaseView.Git_REST_API.API_Objects
+{
+    class Branch
+    {
+    }
+}
