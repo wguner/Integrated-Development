3a7a18fbd214ec2fba16315fd0fcad8eee5cf607
Malachi Potts <malachi.potts@wsu.edu>
2021-05-07 15:44:03 -0700

    Added more unit tests

--- CodebaseView/IDT_Tests/UnitTest1.cs
+++ CodebaseView/IDT_Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using CodebaseView;
+using System.Reflection;
 
 namespace IDT_Tests
 {
@@ -8,11 +9,6 @@ namespace IDT_Tests
     {
         public const string realRepoURL = "https://gitlab.eecs.wsu.edu/2019080728/cpts-421.git";
         public const string repoName = "cpts-421";
-        [SetUp]
-        public void Setup()
-        {
-            
-        }
 
         [Test]
         public void RepoURLTest()
@@ -23,11 +19,83 @@ namespace IDT_Tests
         }
 
         [Test]
-        public void Test2()
+        public void repoNameTest()
         {
             GitParser gitParser = new GitParser();
             string parsedName = gitParser.parseNameFromURL(realRepoURL);
             Assert.AreEqual(repoName, parsedName);
         }
+
+        [Test]
+        public void SelectQuery1()
+        {
+            string testQuery = "SELECT commit_id FROM Commit";
+            string query = new SELECTQueryBuilder()
+                .setColumns("commit_id").setTables("Commit").build();
+            Assert.AreEqual(query, testQuery);
+        }
+
+        [Test]
+        public void SelectQuery2()
+        {
+            string testQuery = "SELECT author_id FROM Author";
+            string queryAuthorID = new SELECTQueryBuilder().setTables("Author")
+                .setColumns("author_id").build();
+            Assert.AreEqual(queryAuthorID, testQuery);
+        }
+
+        [Test]
+        public void SelectQuery3()
+        {
+            string testQuery = "SELECT commit_id FROM Commit";
+            string queryCommitID = new SELECTQueryBuilder().setTables("Commit")
+                        .setColumns("commit_id").build();
+            Assert.AreEqual(queryCommitID, testQuery);
+        }
+
+        [Test]
+        public void SelectQuery4()
+        {
+            string testQuery = "SELECT branch_id FROM Branch";
+            string queryBranchID = new SELECTQueryBuilder().setTables("Branch")
+                            .setColumns("branch_id").build();
+            Assert.AreEqual(queryBranchID, testQuery);
+        }
+
+        [Test]
+        public void SelectQuery5()
+        {
+            string testQuery = "SELECT file_id FROM File";
+            string queryFileID = new SELECTQueryBuilder().setTables("File")
+                            .setColumns("file_id").build();
+            Assert.AreEqual(queryFileID, testQuery);
+        }
+
+        [Test]
+        public void InsertQuery1()
+        {
+            string testQuery = "INSERT INTO Repository() VALUES ()";
+            INSERTQueryBuilder repoInsert = new INSERTQueryBuilder().setTable("Repository");
+            string insertQuery = repoInsert.build();
+            Assert.AreEqual(insertQuery, testQuery);
+        }
+
+        [Test]
+        public void InsertQuery2()
+        {
+            string testQuery = "INSERT INTO Author() VALUES ()";
+            INSERTQueryBuilder authorInsert = new INSERTQueryBuilder().setTable("Author");
+            string insertQuery = authorInsert.build();
+            Assert.AreEqual(testQuery, insertQuery);
+        }
+
+        [Test]
+        public void InsertQuery3()
+        {
+            string testQuery = "INSERT INTO Commit_Map_Branch() VALUES ()";
+            INSERTQueryBuilder branchCommitMapInsert = new INSERTQueryBuilder().setTable("Commit_Map_Branch");
+            string insertBranchCommitMap = branchCommitMapInsert.build();
+            Assert.AreEqual(testQuery, insertBranchCommitMap);
+        }        
     }
 }
